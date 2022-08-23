c82b4adf6d4b59280f08810e3c8f41d692204da7
HelithaSri <helitha.pravin@gmail.com>
2022-08-23 15:29:44 +0530

    use localstorage to save and get user

--- src/app/App.js
+++ src/app/App.js
@@ -5,19 +5,24 @@ import Dashboard from "../pages/Dashboard";
 import Cart from "../pages/Cart";
 import Register from "../pages/Register";
 import NavBar from "../component/common/NavBar";
+import localStorageService from "../service/localStorageService";
 
 function App() {
     return (<>
-            <NavBar/>
-            <Routes>
-                <Route exact path="/" element={<LoginAdmin/>}/>
+            <header style={{display:localStorageService.getItem("user")?"block":"none"}}>
+                <NavBar/>
+            </header>
+            <main>
+                <Routes>
+                    <Route exact path="/" element={<LoginAdmin/>}/>
 
-                {/*<Route exact path="/" element={<LoginAdmin/>}/>*/}
-                <Route path="/dashboard" element={<Dashboard/>}/>
-                <Route path="/product" element={<Product/>}/>
-                <Route path="/cart" element={<Cart/>}/>
-                <Route path="/register" element={<Register/>}/>
-            </Routes>
+                    {/*<Route exact path="/" element={<LoginAdmin/>}/>*/}
+                    <Route path="/dashboard" element={<Dashboard/>}/>
+                    <Route path="/product" element={<Product/>}/>
+                    <Route path="/cart" element={<Cart/>}/>
+                    <Route path="/register" element={<Register/>}/>
+                </Routes>
+            </main>
         </>
 
     );
--- src/pages/session/Login/index.jsx
+++ src/pages/session/Login/index.jsx
@@ -1,136 +1,151 @@
-import { Grid, Typography } from "@mui/material";
-import React, { Component } from "react";
-import { TextValidator, ValidatorForm } from "react-material-ui-form-validator";
-import { styleSheet } from "./style";
-import { withStyles } from "@mui/styles";
-import { Link } from "react-router-dom";
+import {Grid, Typography} from "@mui/material";
+import React, {Component} from "react";
+import {TextValidator, ValidatorForm} from "react-material-ui-form-validator";
+import {styleSheet} from "./style";
+import {withStyles} from "@mui/styles";
+import {Link} from "react-router-dom";
 import CommonButton from "../../../component/common/Button";
 import localStorageService from "../../../service/localStorageService";
+import LoginService from "../../../service/LoginService";
+import jwt_decode from "jwt-decode";
+
 
 class LoginAdmin extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      formData: {
-        userName: "",
-        password: "",
-      },
-    };
-  }
-
-  handleSubmit = async () => {
-    console.log("Hi handle");
-    console.log(this.state.formData);
-    // localStorage.setItem("user",this.state.userName)
-    localStorageService.setItem("user",this.state.userName)
-  };
-
-  handleChange = (event) => {
-    let id = event.target.name;
-    switch (id) {
-      case "userName":
-        const userName = event.target.value;
-        this.setState(
-          Object.assign(this.state.formData, { userName: userName })
-        );
-        // this.setState({ userId });
-        break;
-
-      case "password":
-        const password = event.target.value;
-        this.setState(
-          Object.assign(this.state.formData, { password: password })
-        );
-        break;
-
-      default:
-        break;
+    constructor(props) {
+        super(props);
+        this.state = {
+            formData: {
+                username: "",
+                password: "",
+            },
+        };
     }
-  };
 
-  checkValidity(){
+    handleSubmit = async () => {
+        console.log("Hi handle");
+        console.log(this.state.formData);
 
-  }
+        let fData = this.state.formData
+        let res = await LoginService.postLogin(fData);
+        if (res.status===200){
+            let token = res.data.token;
+            let decoded = jwt_decode(token);
+            localStorageService.setItem("user", decoded.user)
+            console.log(decoded)
+        }
 
-  render() {
-    const { classes } = this.props;
-    return (
-      <Grid
-        container
-        direction={"row"}
-        justifyContent="center"
-        alignItems="stretch"
-        className="h-screen min-h-min w-screen p-0 m-0 bg-login-img bg-no-repeat bg-bottoms bg-cover bg-center bg-blend-overlay bg-stone-700"
-      >
-        <Grid
-          container
-          direction={"row"}
-          item
-          xs={12}
-          md={12}
-          lg={12}
-          justifyContent="center"
-          alignItems="center"
-        >
-          <div
-            className="flex gap-6 flex-col h-fit w-96 rounded-xl bg-blue-300 p-10 m-5 md:m-0 bg-opacity-10  backdrop-blur-sm"
-            style={{ border: "1px solid rgba(255, 255, 255, 0.09)" }}
-          >
-            <Grid item>
-              <Typography
-                variant="h4"
-                className="text-center uppercase font-bold text-white"
-              >
-                Login
-              </Typography>
-            </Grid>
-            <ValidatorForm
-              onSubmit={this.handleSubmit}
-              onError={(errors) => console.log(errors)}
+        // localStorage.setItem("user",this.state.username)
+
+    };
+
+    handleChange = (event) => {
+        let id = event.target.name;
+        switch (id) {
+            case "username":
+                const username = event.target.value;
+                this.setState(
+                    Object.assign(this.state.formData, {username: username})
+                );
+                // this.setState({ userId });
+                break;
+
+            case "password":
+                const password = event.target.value;
+                this.setState(
+                    Object.assign(this.state.formData, {password: password})
+                );
+                break;
+
+            default:
+                break;
+        }
+    };
+
+    checkValidity() {
+
+    }
+
+    render() {
+        const {classes} = this.props;
+        return (
+            <Grid
+                container
+                direction={"row"}
+                justifyContent="center"
+                alignItems="stretch"
+                className="h-screen min-h-min w-screen p-0 m-0 bg-login-img bg-no-repeat bg-bottoms bg-cover bg-center bg-blend-overlay bg-stone-700"
             >
-              <Grid item container direction={"column"} rowGap="20px">
-                <TextValidator
-                  label="User Name"
-                  onChange={this.handleChange}
-                  name="userName"
-                  value={this.state.formData.userName}
-                  validators={["required"]}
-                  errorMessages={["This field is required"]}
-                  className={[classes.textField, "w-full text-red-500"]}
-                />
-                <TextValidator
-                  label="Password"
-                  onChange={this.handleChange}
-                  name="password"
-                  value={this.state.formData.password}
-                  validators={["required"]}
-                  errorMessages={["This field is required"]}
-                  type={"password"}
-                  className={[classes.textField, "w-full"]}
-                />
-                {/*<Link to="dashboard">*/}
-                  <CommonButton
-                    size="large"
-                    variant="contained"
-                    label="Login"
-                    type="submit"
-                    onClick={() => {
-                      this.checkValidity();
-                    }}
-                    className="text-white w-full bg-blue-500 font-bold tracking-wide"
-                  />
-                {/*</Link>*/}
+                <Grid
+                    container
+                    direction={"row"}
+                    item
+                    xs={12}
+                    md={12}
+                    lg={12}
+                    justifyContent="center"
+                    alignItems="center"
+                >
+                    <div
+                        className="flex gap-6 flex-col h-fit w-96 rounded-xl bg-blue-300 p-10 m-5 md:m-0 bg-opacity-10  backdrop-blur-sm"
+                        style={{border: "1px solid rgba(255, 255, 255, 0.09)"}}
+                    >
+                        <Grid item>
+                            <Typography
+                                variant="h4"
+                                className="text-center uppercase font-bold text-white"
+                            >
+                                Login
+                            </Typography>
+                        </Grid>
+                        <ValidatorForm
+                            onSubmit={this.handleSubmit}
+                            onError={(errors) => console.log(errors)}
+                        >
+                            <Grid item container direction={"column"} rowGap="20px">
+                                <TextValidator
+                                    label="User Name"
+                                    onChange={this.handleChange}
+                                    name="username"
+                                    value={this.state.formData.username}
+                                    validators={["required"]}
+                                    errorMessages={["This field is required"]}
+                                    className={[classes.textField, "w-full text-red-500"]}
+                                />
+                                <TextValidator
+                                    label="Password"
+                                    onChange={this.handleChange}
+                                    name="password"
+                                    value={this.state.formData.password}
+                                    validators={["required"]}
+                                    errorMessages={["This field is required"]}
+                                    type={"password"}
+                                    className={[classes.textField, "w-full"]}
+                                />
+                                {/*<Link to="dashboard">*/}
+                                <CommonButton
+                                    size="large"
+                                    variant="contained"
+                                    label="Login"
+                                    type="submit"
+                                    onClick={() => {
+                                        this.checkValidity();
+                                    }}
+                                    className="text-white w-full bg-blue-500 font-bold tracking-wide"
+                                />
+                                {/*</Link>*/}
 
-                <Typography variant="p" className={'text-white'}>
-                  You are not a member? <Link to={'/register'} className={'font-semibold'}> Register Now</Link>
-                </Typography>
-                
-              </Grid>
-            </ValidatorForm>
-          </div>
-        </Grid>
-      </Grid>
-    );
-  }
+                                <Typography variant="p" className={'text-white'}>
+                                    You are not a member? <Link to={'/register'} className={'font-semibold'}> Register
+                                    Now</Link>
+                                </Typography>
+
+                            </Grid>
+                        </ValidatorForm>
+                    </div>
+                </Grid>
+            </Grid>
+        );
+    }
 }
+
 export default withStyles(styleSheet)(LoginAdmin);
